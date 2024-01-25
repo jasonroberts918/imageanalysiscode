5788e5fe8baacf7d444c9acf8f6e610da3866cc6
jasonroberts918 <157750134+jasonroberts918@users.noreply.github.com>
2024-01-25 20:51:16 +0000

    Create FindAndReplacesOringinallTextFilesInDirectory.py

---
+++ reference_code/python/FindAndReplacesOringinallTextFilesInDirectory.py
@@ -0,0 +1,17 @@
+##find and replace string for all text files in a directory
+##ensure your script is in the same root directory as your files directory
+
+import fnmatch
+import os
+
+#root directory where subdirectories/files are located
+root_directory = r"C:\Analysis_Studies\22-187 Ribon_qp\annotation results_skin"
+
+#find and replace string in each text file in root directory
+for root, dirnames, filenames in os.walk(root_directory):
+  for filename in fnmatch.filter(filenames, '*.txt'):
+    with open(os.path.join(root_directory, filename), "r+") as text_file:
+        texts = text_file.read()
+        texts = texts.replace("resolution #1", "")
+    with open(os.path.join(root_directory, filename), "w") as text_file:
+        text_file.write(texts)
