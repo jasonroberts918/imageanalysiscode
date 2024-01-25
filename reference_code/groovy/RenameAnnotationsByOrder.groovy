101f2f2e9cc0b0c3d24514735bccd333b4926bbf
jasonroberts918 <157750134+jasonroberts918@users.noreply.github.com>
2024-01-25 21:03:55 +0000

    Create RenameAnnotationsByOrder.groovy

---
+++ reference_code/groovy/RenameAnnotationsByOrder.groovy
@@ -0,0 +1,9 @@
+//This script names annotations in the order they were created
+
+//names first annotation A
+first = getAnnotationObjects()
+first[0].setName("A")
+
+//names second annotation B
+second = getAnnotationObjects()
+second[1].setName("B")
