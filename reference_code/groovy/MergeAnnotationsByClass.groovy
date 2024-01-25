5876fc7a390ed3faafea8bf035d47150dfb7420e
jasonroberts918 <157750134+jasonroberts918@users.noreply.github.com>
2024-01-25 21:01:15 +0000

    Create MergeAnnotationsByClass.groovy

---
+++ reference_code/groovy/MergeAnnotationsByClass.groovy
@@ -0,0 +1,10 @@
+//merge annotations by class name e.g. all annotations classed A merge together
+//selectObjectsByClassification("A");
+//mergeSelectedAnnotations()
+selectObjectsByClassification("B");
+mergeSelectedAnnotations()
+resolveHierarchy()
+
+//resset classifiers then run
+//resetDetectionClassifications();
+//runObjectClassifier("DoG_lipid");
